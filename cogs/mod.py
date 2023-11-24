ea3f327c8e1392b8fd66c820e9ddc060fe2f5c5c
_Sukuratchi <66184829+Sukuratchi@users.noreply.github.com>
2023-11-25 01:46:09 +0300

    Add mod cog with initial very basic ban support

---
+++ cogs/mod.py
@@ -0,0 +1,19 @@
+import discord
+from discord import app_commands
+from discord.ext import commands
+
+class Mod(commands.Cog):
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+    
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("Mod cog loaded!")
+
+    @app_commands.command(name="ban", description="Ban command")
+    async def ban(self, interaction: discord.Interaction, member: discord.Member, reason: str):
+        await member.ban(reason=reason)
+        await interaction.response.send_message(f"Done!", ephemeral=True)
+
+async def setup(bot):
+    await bot.add_cog(Mod(bot), guilds=[discord.Object(id=850093371073757194)])
