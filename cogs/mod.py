a0af9473025f7c674b754ac518a3e0f03209a460
_Sukuratchi <66184829+Sukuratchi@users.noreply.github.com>
2023-11-25 15:33:23 +0300

    Add permission check and error handling for ban command

--- cogs/mod.py
+++ cogs/mod.py
@@ -11,9 +11,15 @@ class Mod(commands.Cog):
         print("Mod cog loaded!")
 
     @app_commands.command(name="ban", description="Ban command")
+    @app_commands.checks.has_permissions(ban_members=True)
     async def ban(self, interaction: discord.Interaction, member: discord.Member, reason: str):
         await member.ban(reason=reason)
         await interaction.response.send_message(f"Done!", ephemeral=True)
+
+    @ban.error
+    async def on_ban_error(self, interaction: discord.Interaction, error: app_commands.AppCommandError):
+        if isinstance(error, app_commands.MissingPermissions):
+            await interaction.response.send_message(f"You're not allowed to run this command. Sorry!", ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(Mod(bot), guilds=[discord.Object(id=850093371073757194)])
