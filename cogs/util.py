50cae31e08c7ec610b278ba161a4b0d10edd6028
_Sukuratchi <66184829+Sukuratchi@users.noreply.github.com>
2023-11-25 21:12:39 +0300

    Add a statistics command that shows the info about the bot usage

--- cogs/util.py
+++ cogs/util.py
@@ -1,6 +1,7 @@
-import discord, random
+import discord, random, os, platform, psutil
 from discord import app_commands
 from discord.ext import commands
+from math import floor
 
 class Util(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -18,6 +19,20 @@ class Util(commands.Cog):
     @app_commands.command(name='ping', description='Checks bot ping')
     async def ping(self, interaction: discord.Interaction):
         await interaction.response.send_message(f'Pong! Response is at a blazing fast {round(self.bot.latency * 1000)}ms')
+
+    @app_commands.command(name='stats', description='Shows the stats of the bot')
+    async def stats(self, interaction: discord.Interaction):
+        process = psutil.Process(os.getpid())
+
+        embed = discord.Embed(
+            title=f"{self.bot.user.name} Statistics", color=discord.Color.blurple())
+        embed.set_thumbnail(url=self.bot.user.display_avatar)
+        embed.add_field(name="CPU Usage", value=f"{psutil.cpu_percent()}%")
+        embed.add_field(name="Memory Usage",
+                        value=f"{floor(process.memory_info().rss/1000/1000)} MB")
+        embed.add_field(name="Python Version", value=platform.python_version())
+
+        await interaction.response.send_message(embed=embed)
 
     @commands.Cog.listener()
     async def on_member_join(self, member):
