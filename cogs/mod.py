3c616e6277fe6c7039c12b460ee7e00ad4fc5a0a
_Sukuratchi <66184829+Sukuratchi@users.noreply.github.com>
2023-11-25 22:29:05 +0300

    Add embed to modlog ban and kick commands
    This sends an embed in a modlog channel with the person that got banned/kicked, the person who performed the action, and the reasoning
    TODO: Add support for no reasoning (idk how to do that yet, need to look it up lmao)

--- cogs/mod.py
+++ cogs/mod.py
@@ -10,12 +10,19 @@ class Mod(commands.Cog):
     async def on_ready(self):
         print("Mod cog loaded!")
 
-    @app_commands.command(name="ban", description="Ban command")
-    @app_commands.checks.has_permissions(ban_members=True)
+    @app_commands.command(name = "ban", description = "Ban command")
+    @app_commands.checks.has_permissions(ban_members = True)
     async def ban(self, interaction: discord.Interaction, member: discord.Member, reason: str):
+        embed = discord.Embed(
+            title=f"Member banned!", color=discord.Color.blurple())
+        embed.set_thumbnail(url=member.display_avatar)
+        embed.add_field(name = "Member", value = f"<@!{member.id}>")
+        embed.add_field(name = "Performed by", value=f"<@!{interaction.user.id}>")
+        embed.add_field(name = "Reason", value = reason)
+
         myguild = self.bot.get_guild(850093371073757194)
         modlog = myguild.get_channel(1177953545681649714)
-        await modlog.send(f"{member} has been banned!")
+        await modlog.send(embed=embed)
 
         await member.ban(reason=reason)
         await interaction.response.send_message(f"Done! Banned {member}", ephemeral=True)
@@ -28,9 +35,16 @@ class Mod(commands.Cog):
     @app_commands.command(name="kick", description="Kick command")
     @app_commands.checks.has_permissions(kick_members=True)
     async def kick(self, interaction: discord.Interaction, member: discord.Member, reason: str):
+        embed = discord.Embed(
+            title=f"Member kicked!", color=discord.Color.blurple())
+        embed.set_thumbnail(url=member.display_avatar)
+        embed.add_field(name = "Member", value = f"<@!{member.id}>")
+        embed.add_field(name = "Performed by", value=f"<@!{interaction.user.id}>")
+        embed.add_field(name = "Reason", value = reason)
+
         myguild = self.bot.get_guild(850093371073757194)
         modlog = myguild.get_channel(1177953545681649714)
-        await modlog.send(f"{member} has been kicked out!")
+        await modlog.send(embed=embed)
 
         await member.kick(reason=reason)
         await interaction.response.send_message(f"Done! Kicked {member}", ephemeral=True)
