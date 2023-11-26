a2ff3b4eec04a223f10f06827c35c25d91fa125b
_Sukuratchi <66184829+Sukuratchi@users.noreply.github.com>
2023-11-26 20:14:11 +0300

    Add support for tracking if a twitch channel is live
    (Currently testing but should work:tm:

--- cogs/util.py
+++ cogs/util.py
@@ -1,7 +1,66 @@
 import discord, random, os, platform, psutil
 from discord import app_commands
-from discord.ext import commands
+from discord.ext import commands, tasks
 from math import floor
+
+import requests, os
+from dotenv import load_dotenv
+
+load_dotenv()
+client_secret = os.getenv('clientSecret')
+client_id = os.getenv('clientID')
+
+isLive = False
+
+class Stream:
+
+    def __init__(self, title, streamer, game):
+        self.title = title
+        self.streamer = streamer
+        self.game = game
+        
+# getting the auth token from the twitch API
+def getOAuthToken():
+    body = {
+        'client_id': client_id,
+        'client_secret': client_secret,
+        "grant_type": 'client_credentials'
+    }
+    r = requests.post('https://id.twitch.tv/oauth2/token', body)
+
+    #data output
+    keys = r.json()
+    return keys['access_token']
+
+def checkIfLive(channel):
+    # Calling the twitch api to check if a specific is live
+    url = "https://api.twitch.tv/helix/streams?user_login=" + channel
+    token = getOAuthToken()
+
+    HEADERS = {
+        'Client-ID': client_id,
+        'Authorization': 'Bearer ' + token
+    }
+
+    try:
+        
+        req = requests.get(url, headers=HEADERS)
+        
+        res = req.json()
+
+        if len(res['data']) > 0: # the twitch channel is live
+            data = res['data'][0]
+            title = data['title']
+            streamer = data['user_name']
+            game = data['game_name']
+            thumbnail_url = data['thumbnail_url']
+            stream = Stream(title, streamer, game, thumbnail_url)
+            #stream = Stream(title, streamer, game)
+            return stream
+        else:
+            return "OFFLINE"
+    except Exception as e:
+        return "An error occured: " + str(e)
 
 class Util(commands.Cog):
     def __init__(self, bot: commands.Bot):
@@ -9,6 +68,7 @@ class Util(commands.Cog):
     
     @commands.Cog.listener()
     async def on_ready(self):
+        self.twitchNotif.start()
         print("Utility cog loaded!")
 
     @commands.command()
@@ -45,5 +105,27 @@ class Util(commands.Cog):
         welcome = myguild.get_channel(865340222581899284)
         await welcome.send(random.choice(responses))
 
+    @tasks.loop(seconds = 30)
+    async def twitchNotif(self):
+        #print("Running loop")
+        global isLive
+        stream = checkIfLive("SquChan")
+        if stream != "OFFLINE":
+            #print("Checking if stream is live")
+            if isLive == False:
+                isLive = True
+                myguild = self.bot.get_guild(850093371073757194)
+                modlog = myguild.get_channel(1177953545681649714)
+                await modlog.send("Squ is live!")
+                #print("Channel is live!")
+        else:
+            #print("Checking if stream is not live")
+            if isLive == True:
+                isLive = False
+                myguild = self.bot.get_guild(850093371073757194)
+                modlog = myguild.get_channel(1177953545681649714)
+                await modlog.send("SquChan is not live!")
+                #print("Channel is not live!")
+        
 async def setup(bot):
     await bot.add_cog(Util(bot), guilds=[discord.Object(id=850093371073757194)])
