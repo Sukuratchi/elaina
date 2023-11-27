624a71502fb9616c7d76c779a8e90c19ba1e10dc
_Sukuratchi <66184829+Sukuratchi@users.noreply.github.com>
2023-11-27 17:05:28 +0300

    Add a "loaded" slash command and make "sync" a slash command in main file
    Bot replies with the currently loaded cogs and sync no longer used with a regular command (currently works only sometimes, don't know why)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import discord, os, asyncio, random
+import discord, os, asyncio, random, array
 from dotenv import load_dotenv
 from discord.ext import commands, tasks
 from discord import app_commands
@@ -6,16 +6,20 @@ from discord import app_commands
 load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
+loadedCogs = []
+MY_GUILD = discord.Object(id=850093371073757194)
 
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 intents.messages = True
-bot = commands.Bot(command_prefix='!', intents=intents, application_id='991731064026448043')
+bot = commands.Bot(command_prefix='!', intents=intents, application_id='991731064026448043', guilds=[discord.Object(id=850093371073757194)])
 
 @bot.event
 async def on_ready():
     change_status.start()
+    bot.tree.copy_global_to(guild = MY_GUILD)
+    await bot.tree.sync(guild = MY_GUILD)
     print(f"Ready for action! Logged in as {bot.user}")
 
 @tasks.loop(seconds=45)
@@ -34,16 +38,22 @@ async def change_status():
     else:
         await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="TF2"))
 
-@bot.command()
-async def sync(ctx):
-    fmt = await ctx.bot.tree.sync(guild = ctx.guild)
-    await ctx.send(f"Refreshed {len(fmt)} commands.")
-
+@bot.tree.command(name='sync', description='syncs commands from cogs')
+async def sync(interaction: discord.Interaction):
+    fmt = await bot.tree.sync(guild = MY_GUILD)
+    await interaction.response.send_message(f"Refreshed {len(fmt)} commands.", ephemeral = True)
 
 async def load():
     for file in os.listdir('./cogs'):
         if file.endswith('.py'):
+            global loadedCogs
+            loadedCogs.append(file[:-3])
             await bot.load_extension(f'cogs.{file[:-3]}')
+
+@bot.tree.command(name='loaded', description='Checks what cogs are loaded')
+async def loaded(interaction: discord.Interaction):
+    global loadedCogs
+    await interaction.response.send_message(f"{loadedCogs} are loaded!", ephemeral = True)
 
 async def main():
     await load()
