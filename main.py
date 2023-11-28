7759d305b2dd4dc20ac19f9f82a00b9ad4d7a236
_Sukuratchi <66184829+Sukuratchi@users.noreply.github.com>
2023-11-28 04:01:18 +0300

    Change location of cog loading code. Put cog management behind a role

--- main.py
+++ main.py
@@ -39,27 +39,40 @@ async def change_status():
         await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="TF2"))
 
 @bot.tree.command(name='sync', description='syncs commands from cogs')
+@app_commands.checks.has_role(850093685588885544)
 async def sync(interaction: discord.Interaction):
     fmt = await bot.tree.sync(guild = MY_GUILD)
     await interaction.response.send_message(f"Refreshed {len(fmt)} commands.", ephemeral = True)
 
-async def loadCogs():
-    for file in os.listdir('./cogs'):
-        if file.endswith('.py'):
-            loadedCogs.append(file[:-3])
-            await bot.load_extension(f'cogs.{file[:-3]}')
+@sync.error
+async def on_sync_error(interaction: discord.Interaction, error: app_commands.AppCommandError):
+        if isinstance(error, app_commands.MissingRole):
+            await interaction.response.send_message(f"You're not allowed to run this command. Sorry!", ephemeral=True)
 
 @bot.tree.command(name='loaded', description='Checks what cogs are loaded')
+@app_commands.checks.has_role(850093685588885544)
 async def loaded(interaction: discord.Interaction):
     await interaction.response.send_message(f"{loadedCogs} are loaded!", ephemeral = True)
 
+@loaded.error
+async def on_sync_error(interaction: discord.Interaction, error: app_commands.AppCommandError):
+        if isinstance(error, app_commands.MissingRole):
+            await interaction.response.send_message(f"You're not allowed to run this command. Sorry!", ephemeral=True)
+
 @bot.tree.command(name = 'unload', description = 'unloads a cog')
+@app_commands.checks.has_role(850093685588885544)
 async def unload(interaction: discord.Interaction, cog: str):
     await bot.unload_extension(f"cogs.{cog}")
     loadedCogs.remove(cog)
     await interaction.response.send_message(f"Unloaded {cog}", ephemeral = True)
 
+@unload.error
+async def on_sync_error(interaction: discord.Interaction, error: app_commands.AppCommandError):
+        if isinstance(error, app_commands.MissingRole):
+            await interaction.response.send_message(f"You're not allowed to run this command. Sorry!", ephemeral=True)
+
 @bot.tree.command(name = 'load', description = 'Loads a cog')
+@app_commands.checks.has_role(850093685588885544)
 async def load(interaction: discord.Interaction, cog: str):
     for file in os.listdir('./cogs'):
         if file.startswith(cog):
@@ -67,10 +80,27 @@ async def load(interaction: discord.Interaction, cog: str):
             await bot.load_extension(f'cogs.{file[:-3]}')
             await interaction.response.send_message(f"Loaded {cog}", ephemeral = True)
 
+@load.error
+async def on_sync_error(interaction: discord.Interaction, error: app_commands.AppCommandError):
+        if isinstance(error, app_commands.MissingRole):
+            await interaction.response.send_message(f"You're not allowed to run this command. Sorry!", ephemeral=True)
+
 @bot.tree.command(name = 'reload', description = 'Reloads a cog')
+@app_commands.checks.has_role(850093685588885544)
 async def reload(interaction: discord.Interaction, cog: str):
     await bot.reload_extension(f"cogs.{cog}")
     await interaction.response.send_message(f"Reloaded {cog}!", ephemeral = True)
+
+@reload.error
+async def on_sync_error(interaction: discord.Interaction, error: app_commands.AppCommandError):
+        if isinstance(error, app_commands.MissingRole):
+            await interaction.response.send_message(f"You're not allowed to run this command. Sorry!", ephemeral=True)
+
+async def loadCogs():
+    for file in os.listdir('./cogs'):
+        if file.endswith('.py'):
+            loadedCogs.append(file[:-3])
+            await bot.load_extension(f'cogs.{file[:-3]}')
 
 async def main():
     await loadCogs()
