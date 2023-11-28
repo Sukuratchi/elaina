6619109687b6f11ce495f7139854854beb1eff12
_Sukuratchi <66184829+Sukuratchi@users.noreply.github.com>
2023-11-29 01:03:32 +0300

    Move member join role code to roles cog from util

--- cogs/util.py
+++ cogs/util.py
@@ -28,21 +28,6 @@ class Util(commands.Cog):
         embed.add_field(name="Python Version", value=platform.python_version())
 
         await interaction.response.send_message(embed=embed)
-
-    @commands.Cog.listener()
-    async def on_member_join(self, member):
-        responses = [
-            f"Lets all welcome <@!{member.id}> to the server!",
-            f"Hey look, <@!{member.id}> has joined the server!",
-            f"Hi there <@!{member.id}>! Hope you enjoy your stay here.",
-            f"Welcome to the server, <@!{member.id}>!",
-            f"A new face, <@!{member.id}>, has arrived. Welcome in!"
-        ]
-        myguild = self.bot.get_guild(1116469018019233812)
-        welcome = myguild.get_channel(1178850385901932564)
-        role = myguild.get_role(1179086276935299164)
-        await member.add_roles(role)
-        await welcome.send(random.choice(responses))
         
 async def setup(bot):
     await bot.add_cog(Util(bot), guilds=[discord.Object(id=1116469018019233812)])
