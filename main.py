fb6af716c6e59018a82f9dbe3bfaf6efc0932d66
_Sukuratchi <66184829+Sukuratchi@users.noreply.github.com>
2023-11-29 21:29:49 +0300

    Move status changing to util cog

--- main.py
+++ main.py
@@ -17,26 +17,9 @@ bot = commands.Bot(command_prefix='!', intents=intents, application_id='99173106
 
 @bot.event
 async def on_ready():
-    change_status.start()
     bot.tree.copy_global_to(guild = MY_GUILD)
     await bot.tree.sync(guild = MY_GUILD)
     print(f"Ready for action! Logged in as {bot.user}")
-
-@tasks.loop(seconds=45)
-async def change_status():
-    statusType = random.randint(0, 5)
-    if statusType == 0:
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="some anime"))
-    elif statusType == 1:
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="the rain outside"))
-    elif statusType == 2:
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="vtubers"))
-    elif statusType == 3:
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="booba"))
-    elif statusType == 4:
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="ᵇᵉᵃⁿ ᵇᵉᵃⁿ ᵇᵉᵃⁿ ᵇᵉᵃⁿ ᵇᵉᵃⁿ ᵇᵉᵃⁿ ᵇᵉᵃⁿ ᵇᵉᵃⁿ ᵇᵉᵃⁿ ᵇᵉᵃⁿ"))
-    else:
-        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="TF2"))
 
 @bot.tree.command(name='sync', description='syncs commands from cogs')
 @app_commands.checks.has_role(1120840113170157599)
